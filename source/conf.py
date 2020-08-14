59237e44010d443b16378151d91c4797de667eeb
mercuryconsortium <togo.odbadrakh@furman.edu>
2020-08-14 15:01:57 -0400

    added recommonmark extension

--- source/conf.py
+++ source/conf.py
@@ -52,3 +52,8 @@ html_theme = 'sphinxdoc'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# recommonmark
+from recommonmark.parser import CommonMarkParser
+source_parsers = { '.md' : CommonMarkParser,}
+source_suffix = ['.rst','.md']
