dca478ef1655a856cedcae1b17c9306de9f37089
mercuryconsortium <togo.odbadrakh@furman.edu>
2020-08-14 19:29:40 -0400

    toc

--- source/conf.py
+++ source/conf.py
@@ -31,7 +31,6 @@ release = '0.1.1'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = ['recommonmark','sphinxcontrib.fulltoc']
-extensions_path = ['_extensions']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
